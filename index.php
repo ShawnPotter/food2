c43392173b06f2c7e71e508fe7eab777eca78ac6
Shawn Potter <spotter@mail.greenriver.edu>
2021-01-28 15:21:38 -0500

    added routes to order, order2, and summary

--- index.php
+++ index.php
@@ -20,17 +20,20 @@
 
   //Define a order route
   $f3->route('GET /order', function(){
-    echo "Order Route";
+    $view = new Template();
+    echo $view->render('views/form1.html');
   });
 
   //Define order 2 route
   $f3->route('GET /order2', function(){
-    echo "Order 2 Route";
+    $view = new Template();
+    echo $view->render('views/form2.html');
   });
 
   //Define a summary route
   $f3->route('GET /summary', function(){
-    echo "Summary Route";
+    $view = new Template();
+    echo $view->render('views/summary.html');
   });
 
   //run fat free
